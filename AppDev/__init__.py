87b7d6c5b8b6d595da9f59de6138b4ce6e432eba
BrandonN03 <ngiam.brandon@gmail.com>
2025-02-16 21:13:44 +0800

    idk

--- AppDev/__init__.py
+++ AppDev/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, flash
-#asd
+
 import os
 from Forms import SignUpForm, CreateProductForm, LoginForm
 import shelve, User, Product
