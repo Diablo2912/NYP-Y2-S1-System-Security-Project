c8a4e765bdc7c9327183646a237c77eb052426b4
BrandonN03 <ngiam.brandon@gmail.com>
2025-07-19 19:20:50 +0800

    acc freeze html and func

--- AppDev/__init__.py
+++ AppDev/__init__.py
@@ -1699,6 +1699,11 @@ def login():
                 flash("Account is frozen. Check your email to unfreeze it.", "danger")
                 return redirect(url_for('login'))
 
+
+            if is_account_frozen(user['id']):
+                flash("Account is frozen. Check your email to unfreeze it.", "danger")
+                return redirect(url_for('login'))
+
             # Password validation
             stored_password_hash = user['password']
             if verify_password(password, stored_password_hash):
