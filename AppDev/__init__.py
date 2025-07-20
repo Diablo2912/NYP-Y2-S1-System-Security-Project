f17ed7d9c919db96c7d8923a09b0aac717a16556
Sadev06 <sadevdulnth01@gmail.com>
2025-07-20 16:33:04 +0800

    rebase

--- AppDev/__init__.py
+++ AppDev/__init__.py
@@ -123,12 +123,11 @@ EMAIL_PASSWORD = "wivz gtou ftjo dokp"
 # app.config['MYSQL_DB'] = 'ssp_db'
 # app.config['MYSQL_PORT'] = 3306
 #
-# #SACHIN SQL DB CONFIG
-
+# #SADEV SQL DB CONFIG
 # app.secret_key = 'asd9as87d6s7d6awhd87ay7ss8dyvd8bs'
 # app.config['MYSQL_HOST'] = '127.0.0.1'
-# app.config['MYSQL_USER'] = 'glen'
-# app.config['MYSQL_PASSWORD'] = 'dbmsPa55'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'Pa$$w0rd'
 # app.config['MYSQL_DB'] = 'ssp_db'
 # app.config['MYSQL_PORT'] = 3306
 #
